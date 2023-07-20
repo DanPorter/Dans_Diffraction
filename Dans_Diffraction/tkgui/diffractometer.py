a2d4fa54cdb7d9952e7ec014a4abc361f197da77
Alexander Steppke <asteppke@gmail.com>
2023-07-21 00:07:44 +0200

    enabling scaling of Tk frame

--- Dans_Diffraction/tkgui/diffractometer.py
+++ Dans_Diffraction/tkgui/diffractometer.py
@@ -1454,6 +1454,12 @@ class DiffractometerGui:
         # --- Scan ---
         tk_scan(bottom_left, angle_dict, self.latt, self.det)
 
+        # Configure expansion of Tk frame
+        self.root.columnconfigure(1, weight=1)
+        self.root.columnconfigure(2, weight=1)
+        self.root.rowconfigure(1, weight=3)
+        self.root.rowconfigure(2, weight=1)
+
         self.latt.generate_refs()
         self.det.generate_detector()
 
